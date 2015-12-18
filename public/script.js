b4fe9814087ab2fca6f91df7b31203a19798eb8f
Matthew Costabile <mattcosta7@gmail.com>
2015-12-18 00:22:58 -0500

    Added Lightbox effect

--- public/script.js
+++ public/script.js
@@ -1,15 +1,18 @@
 $(document).ready(function(){
  
-  $('a').hover(function() {
-        $(this).stop().animate({ fontSize : '20px' });
-        },
-        function() {
-        $(this).stop().animate({ fontSize : '16px' });
-        });
+$('a').hover(function() {
+      $(this).stop().animate({ fontSize : '20px' });
+      },
+      function() {
+      $(this).stop().animate({ fontSize : '16px' });
+      });
 
   $('.outerlightbox').hide();
+  
   $('.container img').on('click',function(){
+    $('#imagebox').attr('src',$(this).attr('src'));
     $('.outerlightbox').show();
+    
   })
   $('.outerlightbox').on('click',function(){
     $('.outerlightbox').hide();
